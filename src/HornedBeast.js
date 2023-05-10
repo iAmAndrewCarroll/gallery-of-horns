d65f05f5b5592028a915143bbc4907eab0ebc789
Andrew Thomas Carroll <athomascarroll@gmail.com>
2023-05-10 10:22:15 -0700

    card changes

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -20,7 +20,7 @@ class HornedBeast extends React.Component {
   render() {
     return (
       <div className="card-grid-container">
-      <Card className="card" style={{ width: '18rem' }}>
+      <Card className="card">
         <img src={this.props.imageURL} alt={this.props.title} title={this.props.title} />
         <div className="card-body">
           <h5 className="card-title">{this.props.title}</h5>
