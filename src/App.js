fa8c3fc51fd278b49ac1ed45c4a723122fdd2050
Andrew Thomas Carroll <athomascarroll@gmail.com>
2023-05-09 20:45:14 -0700

    lab 02 stuffs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import Header from "./Header";
-import Footer from "./Footer";
 import Main from "./Main";
+import Footer from "./Footer";
+import './App.css';
+
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Header title="My Horned Beasts" />
+        <Header />
         <Main />
-        <Footer author="Your Name" />
+        <Footer />
       </div>
     );
   }
